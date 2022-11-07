255362683f0fee3365101d1c2aa6d2e8a3224cae
haolu253 <luchihao253@gmail.com>
2022-11-07 17:05:21 +0700

    update

--- js/main.js
+++ js/main.js
@@ -24,3 +24,20 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
 }
+
+
+
+
+
+    for (let i = 0; i < 4; i++) {
+      var section1 = document.getElementById('highlights').getBoundingClientRect();
+      var section2 = document.getElementById('about-us').getBoundingClientRect();
+      var section3 = document.getElementById('our-team').getBoundingClientRect();
+      var section4 = document.getElementById('contact-us').getBoundingClientRect();
+
+      console.log(section1.top, section1.right, section1.bottom, section1.left);
+      console.log(section2.top, section2.right, section2.bottom, section2.left);
+      console.log(section3.top, section3.right, section3.bottom, section3.left);
+      console.log(section4.top, section4.right, section4.bottom, section4.left);
+      
+    }
